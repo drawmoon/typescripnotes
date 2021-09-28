dc2179a49e20630648ff830a6f4a8dbd433d7005
drawmoon <1340260725@qq.com>
2021-09-28 10:14:50 +0800

    逻辑运算符 && ||

---
+++ operator.ts
@@ -0,0 +1,10 @@
+// &&
+
+const inputNames: string[] = undefined;
+const inputName = inputNames && inputNames[0];
+console.log(inputName);
+
+// ||
+
+const inputName2: string = undefined;
+console.log(inputName2 || 'abc');
