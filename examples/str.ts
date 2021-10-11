a9dc3f172cfe6706de6df60724945aedf2fff03a
drawmoon <1340260725@qq.com>
2021-10-11 12:02:22 +0800

    简单的字符串 format

--- examples/str.ts
+++ examples/str.ts
@@ -5,3 +5,14 @@ console.log(str.slice(0, 5)); // abcde
 
 // 提取索引 0 处到索引 -2 处之间的字符
 console.log(str.slice(0, -2)); // abcde
+
+// format
+function strformat(str: string, ...args: string[]) {
+    for (const k in args) {
+        str = str.replace(`{${k}}`, args[k]);
+    }
+    return str;
+}
+
+const msg = 'Hello, {0}!';
+console.log(strformat(msg, 'World'));
