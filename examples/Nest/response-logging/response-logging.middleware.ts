b114d6e2eb6f0ab44f32a3576df2cf2f798686d8
drawmoon <1340260725@qq.com>
2021-11-24 22:15:16 +0800

    NestJs 记录接口响应耗时

---
+++ examples/Nest/response-logging/response-logging.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { Logger } from '@nestjs/common';
+
+function responseLoggingMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const start = Date.now();
+
+  res.on('close', () => {
+    const end = Date.now();
+    const totalElapsedTime = end - start;
+
+    Logger.debug(
+      `Process request '${req.method} - ${req.url}', total elapsed time: ${totalElapsedTime} ms.`,
+    );
+  });
+
+  next();
+}
+
+export { responseLoggingMiddleware };
