11cb6505baaf0f30c13c0ee017e686895d5fbf14
drawmoon <1340260725@qq.com>
2021-12-02 17:00:57 +0800

    Update class.ts

--- examples/class.ts
+++ examples/class.ts
@@ -2,52 +2,97 @@
   类的声明
  */
 class Store {
-  constructor(private name: string) {}
+  // 构造函数
+  constructor(
+    // 私有字段
+    private name: string
+  ) {}
 
-  getName() {
+  // 公共函数
+  public getName(): string {
     return this.name;
+  }
+
+  // 受保护的函数
+  protected getStyle(): string {
+    return '';
+  }
+
+  // 私有函数
+  private getOwner(): string {
+    return '';
   }
 }
 
+// 构造实例
+const store = new Store('Game World');
+
 /*
-  类的继承与函数重写
+  类的继承
  */
 class PhysicalStore extends Store {
+  // 构造函数重写
+  constructor(name: string, public description: string) {
+    super(name);
+  }
+
+  // 函数重写
   getName(): string {
     console.log('PhysicalStore getName');
+
+    // 调用基类的函数
     return super.getName();
   }
 }
 
-const physicalStore = new PhysicalStore('Game World');
-physicalStore.getName();
-
-/*
-  类的继承之构造函数
- */
-class CloudStore extends Store {
-  constructor(name: string, public tag: string) {
-    super(name);
-  }
-}
-
-const cloudStore = new CloudStore('Steam', 'lib');
-cloudStore.getName();
-console.log(cloudStore.tag);
-
 /*
   函数重载
  */
-class UserService {
-  getUser(id: number);
+class UserAccessor {
+  public getPhone(id: number): string;
 
-  getUser(name: string);
+  public getPhone(name: string): string;
 
-  getUser(u: number | string) {
-    console.log(u);
+  public getPhone(option: number | string): string {
+    return '';
   }
 }
 
-const userService = new UserService();
-userService.getUser(1);
-userService.getUser('George');
+const userAccessor = new UserAccessor();
+userAccessor.getPhone(1);
+userAccessor.getPhone('George');
+
+/*
+  抽象类
+ */
+abstract class People {
+  // 抽象字段
+  public abstract name: string;
+
+  // 抽象函数
+  public abstract jog(): void;
+}
+
+/*
+  静态成员
+ */
+class Starter {
+  // 静态字段
+  public static drive: string;
+
+  // 静态函数
+  public static startup(): void {
+
+  }
+}
+
+/*
+  接口实现
+ */
+interface IBackgroundTask {
+
+}
+
+class RefreshBackgroundTask implements IBackgroundTask {
+
+}
